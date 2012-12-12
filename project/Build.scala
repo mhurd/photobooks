a3f92327664a27012981718de8e7a43b02c9ddd1
Mike Hurd <leahcim77@gmail.com>
2012-12-12 21:03:40 +0000

    Added dependency for Casbah, MongoDB is next.

--- project/Build.scala
+++ project/Build.scala
@@ -11,11 +11,14 @@ object ApplicationBuild extends Build {
       "com.twitter" % "finagle-core" % "5.3.22",
       "com.twitter" % "finagle-http" % "5.3.22",
       "commons-codec" % "commons-codec" % "1.7",
-      "joda-time" % "joda-time" % "2.0"
+      "joda-time" % "joda-time" % "2.0",
+      "com.mongodb.casbah" % "casbah_2.9.1" % "2.1.5-1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      resolvers += "Twitter Finagle Resolver" at "http://http://maven.twttr.com"
+      resolvers += "Twitter Finagle Resolver" at "http://maven.twttr.com",
+      resolvers += "snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      resolvers += "releases"  at "https://oss.sonatype.org/content/groups/scala-tools"
     )
 
 }
