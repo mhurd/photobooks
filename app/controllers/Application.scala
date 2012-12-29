23fb5fee140bcc5ed0c6b91ee92fcc1fb47f3f3a
Mike Hurd <leahcim77@gmail.com>
2012-12-29 15:47:06 +0000

    Added google analytics

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,24 +2,27 @@ package controllers
 
 import play.api.mvc._
 import model.BookRepository
+import play.api.Play
 
 object Application extends Controller {
 
   val bookRepository = BookRepository()
+  private val googleAnalyticsCode = Play.current.configuration.getString("google.analytics.code").get
+
 
   def index = Action {
     val start = System.currentTimeMillis()
     Async {
       bookRepository.getBooks() map (res => {
         println("total time: " + (System.currentTimeMillis() - start)/1000 + " seconds")
-        Ok(views.html.index(res.filter(book => book.valid)))
+        Ok(views.html.index(res.filter(book => book.valid), googleAnalyticsCode))
       })
     }
   }
 
   def book(isbn: String) = Action {
     Async {
-      bookRepository.getBook(isbn) map (res => Ok(views.html.book(res.head)))
+      bookRepository.getBook(isbn) map (res => Ok(views.html.book(res.head, googleAnalyticsCode)))
     }
   }
 
