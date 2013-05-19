1d7169875aadcae7ca9df265c356417d4bf0d840
Mike Hurd <leahcim77@gmail.com>
2013-05-19 09:43:43 +0100

    Fix to Future

--- app/controllers/LookupBookController.scala
+++ app/controllers/LookupBookController.scala
@@ -5,45 +5,49 @@ import play.api.libs.concurrent.Execution.Implicits._
 import play.api.data.Form
 import play.api.data.Forms._
 import scala.Some
+import scala.concurrent.Future
+import play.api.mvc.Result
 
 object LookupBookController extends BookController {
 
   case class FindBook(isbn: String)
 
   val lookupBookForm = Form(
-        mapping(
-          "isbn" -> text
-        )(FindBook.apply)(FindBook.unapply)
-      )
+    mapping(
+      "isbn" -> text
+    )(FindBook.apply)(FindBook.unapply)
+  )
 
   def submit() = SecuredAction(adminUserCheck) {
-    implicit request =>
+    implicit request => {
       implicit val user = Some(request.user)
-      lookupBookForm.bindFromRequest.fold(
-        formWithErrors => {
-          Ok(views.html.lookupBook(formWithErrors, googleAnalyticsCode))
-        },
-        form => {
-          Logger.info("Finding book: " + form.isbn)
-          val book = bookRepositoryComponent.bookDataRepository.getBook(form.isbn)
-          book map (books =>
-            books match {
-              case Nil => NotFound
-              case book :: _ => {
-                val filledForm = bookForm.fill(book)
-                Ok(views.html.bookEdit(filledForm, googleAnalyticsCode))
+      Async {
+        lookupBookForm.bindFromRequest.fold[Future[Result]](
+          formWithErrors => {
+            Future(Ok(views.html.lookupBook(formWithErrors, googleAnalyticsCode)))
+          },
+          form => {
+            Logger.info("Finding book: " + form.isbn)
+            val book = bookRepositoryComponent.bookDataRepository.getBook(form.isbn)
+            book map (books =>
+              books match {
+                case Nil => NotFound
+                case book :: _ => {
+                  val filledForm = bookForm.fill(book)
+                  Ok(views.html.bookEdit(filledForm, googleAnalyticsCode))
+                }
               }
-            }
-          )
-          NotFound
-        }
-      )
+              )
+          }
+        )
+      }
+    }
   }
 
   def lookupBook() = SecuredAction(adminUserCheck) {
     implicit request =>
-    implicit val user = Some(request.user)
-    Ok(views.html.lookupBook(lookupBookForm.discardingErrors, googleAnalyticsCode))
+      implicit val user = Some(request.user)
+      Ok(views.html.lookupBook(lookupBookForm.discardingErrors, googleAnalyticsCode))
   }
 
 }
